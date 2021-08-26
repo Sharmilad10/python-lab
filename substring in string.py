bef6435d9c7b143e2926342eaca66b6934278a60
Sharmilad10 <79572379+Sharmilad10@users.noreply.github.com>
2021-08-26 10:53:54 +0530

    Add files via upload

---
+++ substring in string.py
@@ -0,0 +1,15 @@
+
+def check(string, sub_str):
+	if (string.find(sub_str) == -1):
+		print("NO")
+	else:
+		print("YES")
+			
+string = "phython programming"
+sub_str ="phython"
+check(string, sub_str)
+
+ out put
+ ================== RESTART: Z:/20mca/substring in string.py ==================
+YES
+>>> 
